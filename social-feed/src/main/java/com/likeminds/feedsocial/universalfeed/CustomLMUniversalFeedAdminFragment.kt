b8c9b4ccffbaa5f9dbb050e3cb2b4cb8debf40e8
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2024-07-28 21:21:19 +0530

    renamed module

---
+++ social-feed/src/main/java/com/likeminds/feedsocial/universalfeed/CustomLMUniversalFeedAdminFragment.kt
@@ -0,0 +1,5 @@
+package com.likeminds.feedsocial.universalfeed
+
+import com.likeminds.feed.android.core.universalfeed.view.LMFeedUniversalFeedFragment
+
+class CustomLMUniversalFeedAdminFragment : LMFeedUniversalFeedFragment()
