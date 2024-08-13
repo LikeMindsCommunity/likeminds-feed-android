5c36cd5938f77a42c29bf8423278adf7ae37def5
Ishaan Jain <ishaan.jain@likeminds.community>
2024-08-13 19:57:12 +0530

    removed example code

--- video-feed/src/main/java/com/likeminds/feedvideo/InvestClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.likeminds.feedvideo
-
-import com.likeminds.feed.android.core.socialfeed.model.LMFeedPostViewData
-
-interface InvestClickListener {
-    fun onInvestIconClick(postViewData: LMFeedPostViewData)
-}
