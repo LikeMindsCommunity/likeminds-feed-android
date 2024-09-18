4dfbd4c5a4c81bb6caace9c468f4bc2e860e3dd3
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2024-09-18 22:27:15 +0530

    base setup of qna feed done

---
+++ qna-feed/src/main/java/com/example/feedqna/LMQnAFeed.kt
@@ -0,0 +1,10 @@
+package com.example.feedqna
+
+import android.app.Application
+
+class LMQnAFeed : Application() {
+
+    companion object {
+        const val LM_QNA_FEED_TAG = "LMQnAFeed"
+    }
+}
